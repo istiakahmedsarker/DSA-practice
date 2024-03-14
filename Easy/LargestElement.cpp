0b71fad270dcf9eda0e5f4d45827f2cc7ea4d0cf
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-03-14 15:00:26 +0600

    solved the problem in brute force approach

--- Easy/LargestElement.cpp
+++ Easy/LargestElement.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+// ! brute force approach
+
+int sortArr(vector<int>& arr)
+{
+
+    sort(arr.begin(), arr.end());
+    return arr[arr.size() - 1];
+}
+
+
 int main()
 {
-    // ! Optimal approach
+    vector<int> arr1 = {2, 5, 1, 7, 0};
+
+    cout << "largestElem: " << sortArr(arr1);
+    cout << endl;
+    return 0;
+}
+
+// ! Optimal approach
+int main()
+{
     int arr[] = {2, 5, 1, 7, 0};
     int largestElem = 0;
 
     for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
     {
-        if(arr[i] > largestElem){
+        if (arr[i] > largestElem)
+        {
             largestElem = arr[i];
         }
     }
