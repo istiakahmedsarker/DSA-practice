5a29f7e26d8c02667f6ffa95396088508530f0d1
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-03-14 14:51:49 +0600

    solved the problem in optimal solution

--- Easy/LargestElement.cpp
+++ Easy/LargestElement.cpp
@@ -1,7 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
-    // ! Brute force approach
-    
+int main()
+{
+    // ! Optimal approach
+    int arr[] = {2, 5, 1, 7, 0};
+    int largestElem = 0;
+
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+    {
+        if(arr[i] > largestElem){
+            largestElem = arr[i];
+        }
+    }
+
+    cout << "largestElem: " << largestElem;
+    cout << endl;
+    return 0;
 }
