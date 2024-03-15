edc5fbd8da0d1eca0afa222b3e54ec9cb3ac0470
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-03-15 22:33:53 +0600

    [bubblesort] improved the code with optimized ones

--- Easy/BubbleSort.cpp
+++ Easy/BubbleSort.cpp
@@ -4,13 +4,21 @@ using namespace std;
 int bubble_sort(vector<int> & arr){
     for (int i = arr.size() -1; i >= 0; i--)
     {
+        //  for optimization
+        int didSwap = 0;
         for (int j = 0; j <= i -1; j++)
         {
             if(arr[j] > arr[j+1]){
                 int temp = arr[j+1];
                 arr[j+1] = arr[j];
                 arr[j] = temp;
+                //  for optimization
+                didSwap = 1;
             }
+        }
+        //  for optimization
+        if(didSwap == 0){
+            break;
         }
         
     }
