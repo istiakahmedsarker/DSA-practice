9ad102c5dc9156ac9e22d2e8be456fed633e176f
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-03-15 22:16:05 +0600

    [bubbleSort] coded bubble sort

---
+++ Easy/BubbleSort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int bubble_sort(vector<int> & arr){
+    for (int i = arr.size() -1; i >= 0; i--)
+    {
+        for (int j = 0; j <= i -1; j++)
+        {
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+    }
+    
+}
+
+int main() {
+    vector<int> arr1 = {0, 5, 89, 6, 1, 45, 24, 53, 65};
+
+    bubble_sort(arr1);
+
+    cout << "Sorted Array:";
+
+    for (int i = 0; i < arr1.size(); i++)
+    {
+        cout << " " << arr1[i];
+    }
+    cout << endl;
+    return 0;
+}
