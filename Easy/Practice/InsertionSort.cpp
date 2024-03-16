e006a98819ba9c753bfaa27fa4103fbcb0dd6ade
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-03-16 17:35:42 +0600

    fixed logical errors

--- Easy/Practice/InsertionSort.cpp
+++ Easy/Practice/InsertionSort.cpp
@@ -8,8 +8,8 @@ int insertion_sort(vector<int> &arr)
         int j = i;
         while (j > 0 && arr[j - 1] > arr[j])
         {
-            int temp = arr[j-1];
-            arr[j-1] = arr[j];
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
             arr[j] = temp;
             j--;
         }
