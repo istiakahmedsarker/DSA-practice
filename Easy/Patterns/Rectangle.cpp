d23233fec1b3984ce6ead5801ad74e3db915a015
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-03-23 21:48:56 +0600

    solved recursion and other porblems

--- Easy/Patterns/Rectangle.cpp
+++ Easy/Patterns/Rectangle.cpp
@@ -123,7 +123,8 @@ void pattern9(int n)
     pattern8(n);
 }
 
-void pattern10(int n){
+void pattern10(int n)
+{
     for (size_t i = 0; i <= 2 * n - 1; i++)
     {
         int stars = i;
@@ -141,24 +142,27 @@ void pattern10(int n){
     }
 }
 
-void pattern11(int n){
+void pattern11(int n)
+{
     int start = 1;
     for (size_t i = 1; i <= n; i++)
     {
-        if(i % 2 == 0) start = 0;
-        else start = 1;
+        if (i % 2 == 0)
+            start = 0;
+        else
+            start = 1;
         for (size_t j = 1; j <= i; j++)
         {
             cout << start;
             start = 1 - start;
         }
-        
+
         cout << endl;
     }
-    
 }
 
-void pattern12(int n){
+void pattern12(int n)
+{
     for (size_t i = 1; i <= n; i++)
     {
         // first nums
@@ -167,11 +171,11 @@ void pattern12(int n){
             cout << j;
         }
         // spaces
-        for (size_t j = 0; j < (n *2) - (i*2); j++)
+        for (size_t j = 0; j < (n * 2) - (i * 2); j++)
         {
             cout << " ";
         }
-        
+
         // second nums
         for (size_t j = i; j > 0; j--)
         {
@@ -179,10 +183,10 @@ void pattern12(int n){
         }
         cout << endl;
     }
-    
 }
 
-void pattern13(int n){
+void pattern13(int n)
+{
     int counts = 1;
     for (size_t i = 0; i <= n; i++)
     {
@@ -193,12 +197,113 @@ void pattern13(int n){
         }
         cout << endl;
     }
-    
+}
+
+void pattern14(int n)
+{
+    for (size_t i = 0; i <= n; i++)
+    {
+        for (char a = 'A'; a <= 'A' + i; a++)
+        {
+            cout << a;
+        }
+        cout << endl;
+    }
+}
+
+void pattern15(int n)
+{
+    for (size_t i = n; i > 0; i--)
+    {
+        for (char ch = 'A'; ch < 'A' + i; ch++)
+        {
+            cout << ch;
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int n)
+{
+    for (size_t i = 0; i <= n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern17(int n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        // first space
+        for (size_t j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        char ch = 'A';
+        int breakpoint = (2 * i + 1) / 2;
+        for (size_t j = 0; j < 2 * i + 1; j++)
+        {
+            cout << ch;
+            if (j <= breakpoint)
+                ch++;
+            else
+                ch--;
+        }
+
+        // second space
+        for (size_t j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    };
+}
+
+void pattern21(int n)
+{
+    for (size_t i = 0; i <= n; i++)
+    {
+        for (size_t j = 0; j <= n; j++)
+        {
+            if (i == n || j == n || i == 0 || j == 0)
+            {
+                cout << "*";
+            }
+            else
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+void pattern22(int n)
+{
+    for (size_t i = 0; i < 2 * n - 1; i++)
+    {
+
+        for (size_t j = 0; j < 2 * n - 1; j++)
+        {
+            int top = i;
+            int bottom = j;
+            int left = (2 * n - 2) - i;
+            int right = (2 * n - 2) - j;
+            cout << (n - min(min(top, bottom), min(left, right)));
+        }
+        cout << endl;
+    }
 }
 
 int main()
 {
-    pattern13(3);
+    pattern22(6);
     cout << "" << endl;
     return 0;
 }
---
+++ Easy/Recursion/problems.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int problem1(int n)
+{
+    if (n < 0)
+        return 0;
+    cout << n << " ";
+    problem1(n - 1);
+}
+
+int num = 0;
+
+int problem2(int n)
+{
+    if (n == 0)
+        return 0;
+    cout << num << " ";
+    num++;
+    problem2(n - 1);
+}
+
+void problemArray(int i, int n, vector<int> &arr)
+{
+    if (i >= n / 2)
+        return;
+    swap(arr[i], arr[n - i]);
+    problemArray(i + 1, n, arr);
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 2};
+    problemArray(0, arr.size()-1, arr); // Pass arr.size() instead of arr.size() - 1
+    for (int num : arr)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
